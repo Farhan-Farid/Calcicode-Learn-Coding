c007cc1dd8be43c99debc7eee351863d76291a60
Farhan-Farid <farhanfaridkhan2024@gmail.com>
2025-12-03 00:13:51 +0600

    commit 2 (slider work)

--- my-project/src/components/navbar.jsx
+++ my-project/src/components/navbar.jsx
@@ -1,22 +1,62 @@
 import "../index.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function Navbar() {
-    const [hasScrolled, sethasScrolled] = false;
+  const [show, setShow] = useState(true);
+  const [lastScroll, setLastScroll] = useState(0);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const currentScroll = window.scrollY;
+
+      if (currentScroll > lastScroll) {
+        // Scrolling down → hide navbar
+        setShow(false);
+      } else {
+        // Scrolling up → show navbar
+        setShow(true);
+      }
+
+      setLastScroll(currentScroll);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, [lastScroll]);
 
   return (
     <>
-      <div className="navdiv sticky top-0 h-20 w-full bg-gray-800 flex items-center justify-between">
+      <div style={{top: show ? "0" : "-90px", transition: "top 0.3s ease",}} className="navdiv sticky top-0 h-20 w-full bg-gray-800 flex items-center justify-between">
         <div className="logo size-text">
-          <div className="mainlog size-12 bg-gray-950 border border-whitei text-white flex items-center justify-center ml-2 text-2xl font-bold rounded-md" style={{fontFamily:'Pixelify sans'}}>
+          <div
+            className="mainlog size-12 bg-gray-950 border border-whitei text-white flex items-center justify-center ml-2 text-2xl font-bold rounded-md"
+            style={{ fontFamily: "Pixelify sans" }}
+          >
             Ca
           </div>
         </div>
-        <p className="text-white tracking-[10px] text-2xl" style={{ fontFamily: "'Pixelify Sans', 'Rubik', sans-serif" }}>calcicode</p>
+        <p
+          className="text-white tracking-[10px] text-2xl"
+          style={{ fontFamily: "'Pixelify Sans', 'Rubik', sans-serif" }}
+        >
+          calcicode
+        </p>
         <ul className="text-white flex space-x-4">
-          <li style={{fontFamily: "'Pixelify Sans', 'Rubik', sans-serif"}} className="">Home</li>
-          <li style={{fontFamily: "'Pixelify Sans', 'Rubik', sans-serif"}} >Missions</li>
-          <li style={{fontFamily: "'Pixelify Sans', 'Rubik', sans-serif"}} className="pr-3">Paths</li>
+          <li
+            style={{ fontFamily: "'Pixelify Sans', 'Rubik', sans-serif" }}
+            className=""
+          >
+            Home
+          </li>
+          <li style={{ fontFamily: "'Pixelify Sans', 'Rubik', sans-serif" }}>
+            Missions
+          </li>
+          <li
+            style={{ fontFamily: "'Pixelify Sans', 'Rubik', sans-serif" }}
+            className="pr-3"
+          >
+            Paths
+          </li>
         </ul>
       </div>
     </>
--- my-project/src/components/slider.jsx
+++ my-project/src/components/slider.jsx
@@ -1,9 +1,63 @@
+import "../index.css";
+import { useState } from "react";
 function Slider() {
+  const [slide, setSlide] = useState(1);
+
   return (
     <>
-      <div className="slidercont w-full h-screen bg-gray-700  ">
-        <div className="sliderwrapper">
-            <p>hello</p>
+      <div className="slidercont w-full h-300 bg-gray-700  ">
+        <div className="sliderwrapper flex max-w-full max-h-200 ">
+          <button className="text-white absolute bottom-[200px] left-0 text-3xl">&lt;</button>
+          <img
+            style={{
+              display:
+                slide === 1
+                  ? "Block"
+                  : slide === 2
+                  ? "None"
+                  : slide === 3
+                  ? "None"
+                  : "Other",
+            }}
+            src="https://fastly.picsum.photos/id/0/5000/3333.jpg?hmac=_j6ghY5fCfSD6tvtcV74zXivkJSPIfR9B8w34XeQmvU"
+            alt=""
+          />
+          <img
+            style={{
+              display:
+                slide === 2
+                  ? "Block"
+                  : slide === 1
+                  ? "None"
+                  : slide === 3
+                  ? "None"
+                  : "Other",
+            }}
+            className="hidden"
+            src="https://fastly.picsum.photos/id/7/4728/3168.jpg?hmac=c5B5tfYFM9blHHMhuu4UKmhnbZoJqrzNOP9xjkV4w3o"
+            alt=""
+          />
+          <img
+            style={{
+              display:
+                slide === 3
+                  ? "Block"
+                  : slide === 1
+                  ? "None"
+                  : slide === 2
+                  ? "None"
+                  : "Other",
+            }}
+            className="hidden"
+            src="https://fastly.picsum.photos/id/10/2500/1667.jpg?hmac=J04WWC_ebchx3WwzbM-Z4_KC_LeLBWr5LZMaAkWkF68"
+            alt=""
+          />
+          <button
+            onClick={() => setSlide((prev) => (prev === 3 ? 1 : prev + 1))}
+            className="text-white absolute bottom-[200px] right-0 text-3xl"
+          >
+            &gt;
+          </button>
         </div>
       </div>
     </>
